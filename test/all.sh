f0df3a859067921353a46e358619dc39805aae96
Alexandre Couedelo <alexandre.couedelo@desjardins.com>
2018-10-22 16:12:12 -0400

    feat: support variables with spaces
    #<description>
    
    #<footer>
    
    #Types:
    ## build: Système de build (example : gulp, webpack, npm)
    ## ci: Intégration continue (example scopes: Travis, Circle, BrowserStack, SauceLabs)
    ## docs: Documentation
    ## feat: Ajout d'une fonctionnalité
    ## fix: Correction de bogue
    ## perf: Amélioration des performances
    ## refactor: Changement du code qui ne change rien au fonctionnement
    ## style: Changement du style du code (sans changer la logique)
    ## test: Modification des tests
    #Portée:
    ## définit quelle partie de votre librairie / application est affectée par le commit (cette information est optionnelle)

--- test/all.sh
+++ test/all.sh
@@ -199,7 +199,7 @@ test multiple_channels | jq -e "
 test env_file | jq -e "
   .body.text == \"Inline static text\n\" and
   .body.attachments[0].color == \"danger\" and
-  .body.attachments[0].text == \"Build \`my-build\` failed! (1.0.1)\" and
+  .body.attachments[0].text == \"Build \`my-build\` failed! (1.0.1) - Quality Rating: B (ERROR)\" and
   ( .body.attachments | length == 1 )"
 
 echo -e '\e[32;1m'"All tests passed!"'\e[0m' 
