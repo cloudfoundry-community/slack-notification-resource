39020e36b6f413aa260a69e4369fbc13b95062ab
ch-gareth <gedmunds@companieshouse.gov.uk>
2019-12-04 16:52:17 +0000

    Removes unused build scipt. DVOP-767

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -t slack-notify-resource .
-docker tag -f slack-notify-resource 192.168.59.103:5000/slack-notify-resource
-docker push 192.168.59.103:5000/slack-notify-resource
-
